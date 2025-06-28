5d2501dc12c29b12c2eed705a77d825ace63a15a
DuongB2012074 <duongb2012074@student.ctu.edu.vn>
2025-06-28 17:08:30 +0700

    Fix UTF-8 encoding in pytest

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home_route():
     test_client = app.test_client()
     response = test_client.get('/')
     assert response.status_code == 200
-    assert b"Ứng dụng Dự đoán Nấm" in response.data
+    assert "Ứng dụng Dự đoán Nấm" in response.data.decode('utf-8')
